7b9f2432a8b6c6ca82fb86ab5c04d8343ea90eee
AlexMaz99 <56346754+AlexMaz99@users.noreply.github.com>
2020-03-09 15:02:52 -0400

    Update main.c

--- cw01/zad2/main.c
+++ cw01/zad2/main.c
@@ -1,2 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <time.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include "../zad1/library.h"
+
+clock_t startTime, endTime;
+struct tms st_cpu, en_cpu;
+FILE *resultFile;
+
+void startTimer(){
+    startTime = times(&st_cpu);
+}
+void endTimer(){
+    endTime = times(&en_cpu);
+}
+
+void writeResultToFile(FILE *file, char*name){
+    endTimer();
+    double realTime = (double) (endTime - startTime) / sysconf(_SC_CLK_TCK);
+
+}
+
+int main(int argc, char**argv) {
+    
+}
