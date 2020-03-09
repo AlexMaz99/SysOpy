3b85e273333bdd0928d318553a4a25ac8d775840
AlexMaz99 <56346754+AlexMaz99@users.noreply.github.com>
2020-03-09 11:50:18 -0400

    update

--- lab1/library.c
+++ lab1/library.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "library.h"
+//#include "library.h"
 #include <ctype.h>
 
+struct Block{
+    int numberOfOperations;
+    char** operations;
+};
+struct MainArray{
+    int numberOfBlocks; //number of pairs of files
+    struct Block ** blocks;
+};
+
+//tworzenie tablicy głównej
 struct MainArray* createArray(int numberOfBlocks){
     if (numberOfBlocks <= 0) return NULL;
     struct MainArray *mainArray= (struct MainArray*)calloc(1,sizeof(struct MainArray));
     mainArray -> numberOfBlocks = 0;
-    mainArray -> blocks = (char**)calloc(numberOfBlocks, sizeof(char*));
-
+    mainArray -> blocks = (struct Block**)calloc(numberOfBlocks, sizeof(struct Block*));
     return mainArray;
 }
 
+//tworzenie bloku
 struct Block *createBlock(int numberOfOperations){
-  if(numberOfOperations <= 0) return NULL;
+  if(numberOfOperations < 0) return NULL;
   struct Block * block=(struct Block*)calloc(1,sizeof(struct Block));
-  block -> operations=(char**)calloc(numberOfOperations,sizeof(char*));
+  if (numberOfOperations == 0) block -> operations = NULL;
+  else block -> operations=(char**)calloc(numberOfOperations,sizeof(char*));
   block -> numberOfOperations = numberOfOperations;
-
   return block;
 }
 
+//usuwanie bloku o danym indeksie
 void deleteBlock(struct MainArray* mainArray, int index){
     if(index < 0 || mainArray == NULL || mainArray -> blocks[index] == NULL) return;
     for(int i = 0; i < mainArray -> blocks[index] -> numberOfOperations; i++){
@@ -35,6 +46,7 @@ void deleteBlock(struct MainArray* mainArray, int index){
 
 }
 
+//usuwanie operacji o danym indeksie z bloku o danym indeksie
 void deleteOperation(struct MainArray* mainArray, int block_index, int operation_index){
     free(mainArray -> blocks[block_index] -> operations[operation_index]);
     mainArray -> blocks[block_index] -> operations[operation_index] = NULL;
@@ -43,6 +55,7 @@ void deleteOperation(struct MainArray* mainArray, int block_index, int operation
     else mainArray -> blocks[block_index] -> numberOfOperations = numberOfOperations - 1;
 }
 
+//usuwanie tablicy głównej
 void deleteArray(struct MainArray* mainArray){
     for (int i = 0; i < mainArray -> numberOfBlocks; i++){
         deleteBlock(mainArray, i);
@@ -51,6 +64,7 @@ void deleteArray(struct MainArray* mainArray){
     free(mainArray);
 }
 
+//porównanie dwóch plików poleceniem diff i zapisanie wyniku do pliku tymczasowego
 void compareTwoFiles(char* file1, char*file2){
     system("touch tmp.txt");
     char tmp[12 + strlen(file1) + strlen(file2)];
@@ -62,14 +76,22 @@ void compareTwoFiles(char* file1, char*file2){
     system(tmp);
 }
 
+//obliczenie ilości operacji dla danych dwóch plików
 int countOperationsInBlock(char *file1, char *file2){
-  FILE *fp = popen("diff a.txt b.txt | grep -c ^[0-9]","r");
+  char command[20 + strlen(file1) + strlen(file2)];
+  strcpy(command, "diff ");
+  strcat(command, file1);
+  strcat(command, " ");
+  strcat(command, file2);
+  strcat (command, " | grep -c  ^[0-9]");
+
+  FILE *fp = popen(command,"r");
   int counter;
   fscanf(fp,"%d", &counter);
   return counter;
 }
 
-
+//stworzenie bloku i operacji dla zawartości pliku tmp.txt
 struct Block* createBlockAndOperations(char *tmp, int numberOfOperations){
   FILE *f = fopen(tmp, "r");
   if(f == NULL)  exit(0);
@@ -77,6 +99,11 @@ struct Block* createBlockAndOperations(char *tmp, int numberOfOperations){
   size_t len = 0;
 
   struct Block * block = createBlock(numberOfOperations);
+  if(numberOfOperations == 0) {
+    block -> operations = NULL;
+    fclose(f);
+    return block;
+  }
   char operation[256];
   strcpy(operation,"");
   int i = 0;
@@ -85,7 +112,7 @@ struct Block* createBlockAndOperations(char *tmp, int numberOfOperations){
       if(strcmp(operation, "") != 0){
         block -> operations[i]=(char*)calloc(strlen(operation), sizeof(char));
         strcpy(block -> operations[i], operation);
-        printf("%s\n", operation);
+        //printf("%s\n\n", operation);
         i++;
         strcpy(operation ,"");
       }
@@ -96,6 +123,7 @@ struct Block* createBlockAndOperations(char *tmp, int numberOfOperations){
   if(strcmp(operation, "") != 0){ //last editing operation
     block -> operations[i]=(char*)calloc(strlen(operation), sizeof(char));
     strcpy(block -> operations[i], operation);
+    //printf("%s\n\n", operation);
   }
 
   fclose(f);
@@ -103,37 +131,85 @@ struct Block* createBlockAndOperations(char *tmp, int numberOfOperations){
   return block;
 }
 
-void processFiles(char *files[], int size, struct MainArray *mainArray){
+//zamiana stringa z plikami na tablicę stringów
+void definePairSequence(char* files, char**newFiles)
+{ // file1A.txt:file1B.txt file2A.txt:file2B.txt
+
+    if(files == NULL || strlen(files) == 0) exit (0); 
+    char *filesCpy = calloc(strlen(files), sizeof(char));
+    strcpy(filesCpy, files);
+    char *ptr = strtok(filesCpy, ": ");
+    char *file1, *file2;
+    int index = 0;
+
+    while (ptr != NULL)
+    {
+        file1 = ptr;
+        ptr = strtok(NULL, ": ");
+        newFiles[index] = file1;
+        index ++;
+
+        if(ptr == NULL) exit (-1);
+        file2 = ptr;
+        ptr = strtok(NULL, ": ");
+        newFiles[index] = file2;
+        index++;
+
+    }
+}
+
+//porównanie wszystkich par plików i utworzenie odpowiednich struktur
+void comparePairs(char *filesInString, int size, struct MainArray *mainArray){
   if (size%2 != 0) exit(0);
+  char** files = (char**)calloc(size, sizeof(char*));
+  definePairSequence(filesInString, files);
+
   int index = 0;
   for (int i = 0; i< size-1; i+=2){
-    int numberOfOperations = countOperationsInBlock(files[i], files[i+1]);
     compareTwoFiles(files[i], files[i+1]);
+    int numberOfOperations = countOperationsInBlock(files[i], files[i+1]);
     struct Block* block = createBlockAndOperations("tmp.txt", numberOfOperations);
+  
     system("rm tmp.txt");
 
-    // for (int i = 0; i < mainArray->numberOfBlocks; i++) {
-    //     if (mainArray -> blocks[i] != NULL) {
-    //         index = i;
-    //         break;
-    //     }
-    // }
-    // if (index == -1) {
-    //     return;
-    // }
     mainArray -> blocks[index] = block;
     index ++;
     mainArray -> numberOfBlocks++;
   }
 }
 
+//liczba operacji dla bloku o danym indeksie
+int getNumberOfOperations(struct MainArray* mainArray, int indexOfBlock){
+  if (mainArray == NULL || mainArray -> blocks[indexOfBlock] == NULL) return 0;
+  return mainArray -> blocks[indexOfBlock] -> numberOfOperations;
+}
+
+//operacja o danym indeksie z danego bloku
+char* getOperation(struct MainArray* mainArray, int indexOfBlock, int indexOfOperation){
+  return mainArray -> blocks[indexOfBlock] -> operations[indexOfOperation];
+}
 
 int main(){
-    struct MainArray* mainArray = createArray(2);
-    char*a = "a.txt";
-    char*b = "b.txt";
-    char*c = "c.txt";
-    char*d = "d.txt";
-    char* files[4] = {a, b, c, d};
-    process_files(files, 4, mainArray);
+    struct MainArray* mainArray = createArray(3);
+    int size = 6;
+
+    comparePairs("a.txt:b.txt c.txt:d.txt e.txt:f.txt", size, mainArray);
+
+    printf("%d\n\n", mainArray->blocks[0]->numberOfOperations);
+    printf("%s\n\n", mainArray->blocks[0]->operations[0]);
+    printf("%s\n\n", mainArray->blocks[0]->operations[1]);
+    printf("%s\n\n", mainArray->blocks[0]->operations[2]);
+    printf("%d\n\n", mainArray->blocks[1]->numberOfOperations);
+    printf("%s\n\n", mainArray->blocks[1]->operations[0]);
+    printf("%s\n\n", mainArray->blocks[1]->operations[1]);
+    printf("%s\n\n", mainArray->blocks[1]->operations[2]);
+    
+    deleteBlock(mainArray, 0);
+    if (mainArray -> blocks[0] == NULL) printf("block 0 deleted\n");
+    deleteBlock(mainArray, 1);
+    if (mainArray -> blocks[1] == NULL) printf("block 1 deleted\n");
+    deleteBlock(mainArray, 2);
+    if (mainArray -> blocks[2] == NULL) printf("block 2 deleted\n");
+
+    return 0;
 }
