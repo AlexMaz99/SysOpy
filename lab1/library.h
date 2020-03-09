d3161d99e20b0f633aac77cc8eb8878a1511c18f
AlexMaz99 <56346754+AlexMaz99@users.noreply.github.com>
2020-03-09 11:57:06 -0400

    Update library.h

--- lab1/library.h
+++ lab1/library.h
@@ -1,13 +1,39 @@
 #ifndef LAB1_LIBRARY_H
 #define LAB1_LIBRARY_H
 
+
 struct Block{
     int numberOfOperations;
     char** operations;
 };
+
 struct MainArray{
     int numberOfBlocks; //number of pairs of files
     struct Block ** blocks;
 };
+
+struct MainArray* createArray(int numberOfBlocks);
+
+struct Block *createBlock(int numberOfOperations);
+
+void deleteBlock(struct MainArray* mainArray, int index);
+
+void deleteOperation(struct MainArray* mainArray, int block_index, int operation_index);
+
+void deleteArray(struct MainArray* mainArray);
+
+void compareTwoFiles(char* file1, char*file2);
+
+int countOperationsInBlock(char *file1, char *file2);
+
+struct Block* createBlockAndOperations(char *tmp, int numberOfOperations);
+
+void definePairSequence(char* files, char**newFiles);
+
+void comparePairs(char *filesInString, int size, struct MainArray *mainArray);
+
+int getNumberOfOperations(struct MainArray* mainArray, int indexOfBlock);
+
+char* getOperation(struct MainArray* mainArray, int indexOfBlock, int indexOfOperation);
 
 #endif //LAB1_LIBRARY_H
