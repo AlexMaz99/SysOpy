09d26ba0ec65fb2c977339ecd6d3526e346fd2c4
AlexMaz99 <56346754+AlexMaz99@users.noreply.github.com>
2020-03-09 17:55:36 -0400

    Update main.c

--- cw01/zad2/main.c
+++ cw01/zad2/main.c
@@ -3,31 +3,36 @@
 #include <string.h>
 #include <sys/resource.h>
 #include <time.h>
-#include <sys/time.h>
+#include <sys/times.h>
 #include <unistd.h>
-#include "../zad1/library.h"
+#include "library.h"
 
 clock_t startTime, endTime;
-struct tms st_cpu, en_cpu;
+struct tms startCpu, endCpu;
 
 FILE *resultFile;
 
 void startTimer(){
-    startTime = times(&st_cpu);
+    startTime = times(&startCpu);
 }
 void endTimer(){
-    endTime = times(&en_cpu);
+    endTime = times(&endCpu);
 }
 
-void writeResultToFile(){
+void writeResultToFile(FILE* file, char* name){
     endTimer();
     double realTime = (double) (endTime - startTime) / sysconf(_SC_CLK_TCK);
-    double userTime = (double) (en_cpu.tms_utime - st_cpu.tms_utime) / sysconf(_SC_CLK_TCK);
-    double systemTime = (double)(en_cpu.tms_stime - st_cpu.tms_stime) / sysconf(_SC_CLK_TCK);
-    printf("%f", realTime);
-    printf("%f", userTime);
-    printf("%f", systemTime);
+    double userTime = (double) (endCpu.tms_utime - startCpu.tms_utime) / sysconf(_SC_CLK_TCK);
+    double systemTime = (double)(endCpu.tms_stime - startCpu.tms_stime) / sysconf(_SC_CLK_TCK);
+    fprintf(file, "Operation: %s\n", name);
+    fprintf(file, "Real time: %f\n", realTime);
+    fprintf(file, "User time: %f\n", userTime);
+    fprintf(file, "System time: %f\n\n", systemTime);
 
+    printf("Operation: %s\n", name);
+    printf("Real time: %f\n", realTime);
+    printf("User time: %f\n", userTime);
+    printf("System time: %f\n\n", systemTime);
 }
 
 
@@ -42,30 +47,37 @@ int main(int argc, char**argv) {
         return -1;
     }
     
-    //resultFile = fopen("raport2.txt", "a");
+    resultFile = fopen("raport.txt", "a");
 
     int numberOfBlocks = atoi(argv[2]);
     struct MainArray* mainArray = createArray(numberOfBlocks);
     int i = 3;
+    char* operation;
     while (i < argc){
         startTimer();
-
+        
         if (!strcmp(argv[i], "compare_pairs")){
             comparePairs(argv[i+1], numberOfBlocks * 2, mainArray);
             i += 2;
+            operation = "compare_pairs";
         }
         else if (!strcmp(argv[i], "remove_operation")){
             int indexOfBlock = atoi(argv[i + 1]);
             int indexOfOperation = atoi(argv[i + 2]);
             deleteOperation(mainArray, indexOfBlock, indexOfOperation);
             i += 3;
+            operation = "remove_operation";
         }
         else if (!strcmp(argv[i], "remove_block")){
             int indexOfBlock = atoi(argv[i + 1]);
             deleteBlock(mainArray, indexOfBlock);
             i += 2;
+            operation = "remove_block";
         }
-        writeResultToFile();
+        else{
+            operation = "wrong operation";
+        }
+        writeResultToFile(resultFile, operation);
     }
     deleteArray(mainArray);
     return 0;
