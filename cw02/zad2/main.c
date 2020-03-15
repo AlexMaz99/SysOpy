d93b2c82b9fa40c5e6241ab767f56eb27de829ad
AlexMaz99 <ola_m10@onet.pl>
2020-03-15 13:43:43 -0400

    correct, but I'm not sure if mtime and atime works

--- cw02/zad2/main.c
+++ cw02/zad2/main.c
@@ -42,6 +42,31 @@ void print_results(char* file_path, struct stat *stat){
 
 }
 
+void print_results_nftw(const char* file_path, const struct stat *stat){
+    char file_type[64] = "undefined";
+    if (S_ISREG(stat -> st_mode)) strcpy(file_type, "file");
+    else if (S_ISDIR(stat -> st_mode)) strcpy(file_type, "dir");
+    else if (S_ISLNK(stat -> st_mode)) strcpy(file_type, "slink");
+    else if (S_ISCHR(stat -> st_mode)) strcpy(file_type, "char dev");
+    else if (S_ISBLK(stat -> st_mode)) strcpy(file_type, "block dev");
+    else if (S_ISFIFO(stat -> st_mode)) strcpy(file_type, "fifo");
+    else if (S_ISSOCK(stat -> st_mode)) strcpy(file_type, "socket");
+
+    struct tm tm_modif_time;
+    localtime_r(&stat -> st_mtime, &tm_modif_time);
+    char modif_time_str[255];
+    strftime(modif_time_str, 255, format, &tm_modif_time);
+
+    struct tm tm_access_time;
+    localtime_r(&stat -> st_atime, &tm_access_time);
+    char access_time_str[255];
+    strftime(access_time_str, 255, format, &tm_access_time);
+
+    printf("%s || type: %s, size: %ld, modification time: %s, access time: %s, nlinks: %ld\n\n",
+    file_path, file_type, stat -> st_size, modif_time_str, access_time_str, stat -> st_nlink);
+
+}
+
 int check_time(int count, char sign, time_t time_from_file){
     time_t now;
     struct tm *time_info;
@@ -50,7 +75,7 @@ int check_time(int count, char sign, time_t time_from_file){
     time_t current_date = mktime(time_info);
 
     int diff = difftime(current_date, time_from_file) / 86400; // (24h * 60min * 60s)
-    //printf("Diff: %d\n", diff);
+    printf("Diff: %d, count: %d, sign: %c\n", diff, count, sign);
     if ((sign == '+' && diff > count) || (sign == '-' && diff < count) || (sign == '=' && diff == count)) 
         return 1;
 
@@ -58,6 +83,12 @@ int check_time(int count, char sign, time_t time_from_file){
 }
 
 int check_conditions(struct stat* dir_stat){
+    if (atime != -1 && check_time(atime, asign, dir_stat -> st_atime) == 0) return 0;
+    if (mtime != -1 && check_time(mtime, msign, dir_stat -> st_mtime) == 0) return 0;
+    return 1;
+}
+
+int check_conditions_nftw(const struct stat* dir_stat){
     if (atime != -1 && check_time(atime, asign, dir_stat -> st_atime) == 0) return 0;
     if (mtime != -1 && check_time(mtime, msign, dir_stat -> st_mtime) == 0) return 0;
     return 1;
@@ -103,15 +134,10 @@ void find_dir(char *path, int depth){
 }
 
 static int find_nftw(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf){
-    if (ftwbuf -> level > max_depth) return 1;
-    if (check_conditions(sb) == 1){
-        print_results(fpath, sb);
+    if (ftwbuf -> level > max_depth) return 0;
+    if (check_conditions_nftw(sb) == 1){
+        print_results_nftw(fpath, sb);
     }
-    // else{
-    //     printf("TO NIE:");
-    //     print_results(fpath, sb);
-    // }
-    
     return 0;
 }
 
