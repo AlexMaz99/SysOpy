65e94fda4e065cf1c01184aece48b5ddde48922f
AlexMaz99 <ola_m10@onet.pl>
2020-03-21 12:39:34 +0100

    zad1 works

--- cw03/zad1/main.c
+++ cw03/zad1/main.c
@@ -10,23 +10,55 @@
 #include <time.h>
 #include <dirent.h>
 
-void find_dir(){
+void find_dir(char *path){
 
-    DIR* dir = opendir(".");
+    if (path == NULL) return;
+    DIR* dir = opendir(path);
     if (dir == NULL){
-        printf("Cannot open directory");
+        printf("Cannot open directory\n");
         exit(EXIT_FAILURE);
     }
 
-    struct dirent *file;
+    struct stat buffer;
+    lstat(path, &buffer);
+    if (S_ISDIR(buffer.st_mode)){
+        pid_t pid_fork = fork();
 
+        if (pid_fork < 0){
+            printf("Cannot fork\n");
+            exit(EXIT_FAILURE);
+        }
+
+        else if(pid_fork == 0){
+            
+            // get process ID (PID) of the calling process
+            printf("DIRECTORY: %s, PID: %d\n", path, getpid());
+            
+            // replace the current process image with a new process image
+            int exec_status = execlp("ls", "ls", "-l", path, NULL);
+            if (exec_status != 0){
+                printf("Exec failed");
+                exit(EXIT_FAILURE);
+            }
+            exit(exec_status);
+        }
+
+        else{
+            wait(0);
+        }
+    }
+    
+    
+    struct dirent *file;
+    char new_path[256];
     while ((file = readdir(dir)) != NULL){
-        
-        struct stat buffer;
+        strcpy(new_path, path);
+        strcat(new_path, "/");
+        strcat(new_path, file -> d_name);
 
         // check information about a file
-        if (lstat(file -> d_name, &buffer) < 0){
-            printf("Cannot lstat file %s: ", file -> d_name);
+        if (lstat(new_path, &buffer) < 0){
+            printf("Cannot lstat file %s: ", new_path);
             exit(EXIT_FAILURE);
         }
         
@@ -36,53 +68,8 @@ void find_dir(){
             if (strcmp(file -> d_name, ".") == 0 || strcmp(file -> d_name, "..") == 0){
                 continue;
             }
-
-            if (chdir(file -> d_name) != 0){
-                printf("Cannot change path to %s\n", file -> d_name);
-                exit(EXIT_FAILURE);
-            }
-
-            find_dir();
-
-            pid_t pid_fork = fork();
-
-            if (pid_fork < 0){
-                printf("Cannot fork\n");
-                exit(EXIT_FAILURE);
-            }
-
-            else if(pid_fork == 0){
-                char cwd[4096];
-
-                // get the pathname of the current working directory
-                if (getcwd(cwd, 4096) == NULL){
-                    printf("Cannot get current working directory\n");
-                    exit(EXIT_FAILURE);
-                }
-                
-                // get process ID (PID) of the calling process
-                printf("DIRECTORY: %s, PID: %d\n", cwd, getpid());
-
-                // replace the current process image with a new process image
-                int exec_status = execlp("ls", "ls", "-l", NULL);
-                if (exec_status != 0){
-                    printf("Exec failed");
-                    exit(EXIT_FAILURE);
-                }
-                exit(exec_status);
-            }
-
-            else{
-                wait(0);
-            }
-
-            // change path
-            if  (chdir("..") != 0){
-                printf("Cannot change path to %s", file -> d_name);
-                exit(EXIT_FAILURE);
-            }
+            find_dir(new_path);
         }
-        
     }
     closedir(dir);
 }
@@ -93,12 +80,9 @@ int main(int argc, char** argv){
         exit(EXIT_FAILURE);
     }
 
-    if (chdir(argv[1]) != 0){
-        printf("Cannot chmod to %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+    char* path = argv[1];
 
-    find_dir();
+    find_dir(path);
 
     return 0;
 }
