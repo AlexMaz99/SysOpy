b8356798fd7bc85bd33e583f723b3922d2f5256f
AlexMaz99 <ola_m10@onet.pl>
2020-03-24 16:43:38 +0100

    probably finished, but paste not always works ...

--- cw03/zad2/macierz.c
+++ cw03/zad2/macierz.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/file.h>
+#include <sys/wait.h>
 #include <time.h>
 #include <fcntl.h>
 #include <dirent.h>
@@ -23,6 +24,7 @@ typedef struct{
 Matrix **AA;
 Matrix **BB;
 Matrix **CC;
+
 int numberOfPairs;
 const int MAX_ABS_VAL = 100;
 const int MAX_ROW_LEN = 100000;
@@ -104,12 +106,12 @@ void fillMatrixFile(FILE *file, Matrix *C){
 }
 
 // create result of multiplying Matrix
-Matrix* createResultMatrix(char *path, Matrix *A, Matrix *B){
+Matrix* createResultMatrix(char *path, int numberOfRows, int numberOfColumns){
     Matrix *matrix = malloc(sizeof(Matrix));
     matrix -> path = calloc(FILENAME_MAX, sizeof(char));
     strcpy(matrix -> path, path);
-    matrix -> numberOfRows = A -> numberOfRows;
-    matrix -> numberOfColumns = B -> numberOfColumns;
+    matrix -> numberOfRows = numberOfRows;
+    matrix -> numberOfColumns = numberOfColumns;
     matrix -> widthOfColumn = (int) log10(MAX_ABS_VAL * MAX_ABS_VAL) + 3;
     FILE *file = fopen(matrix -> path, "w+");
     fillMatrixFile(file, matrix);
@@ -122,6 +124,7 @@ void freeMatrix(Matrix *m){
     free(m -> path);
     free(m);
 }
+
 // remove matrices
 void freeList(){
     for (int i = 0; i < numberOfPairs; i ++){
@@ -159,7 +162,7 @@ void readList(char *lista){
         char* C = strtok(NULL, "\n");
         AA[counter] = readMatrix(A);
         BB[counter] = readMatrix(B);
-        CC[counter] = createResultMatrix(C, AA[counter], BB[counter]);
+        CC[counter] = createResultMatrix(C, AA[counter] -> numberOfRows, BB[counter] -> numberOfColumns);
         counter++;
     }
 
@@ -176,9 +179,11 @@ int getValueAtPosition(Matrix *matrix, int row, int column, FILE *file){
     fseek(file, 0, 0);
     int counter = 0;
     char* buffer = calloc(MAX_ROW_LEN, sizeof(char));
+
     while(fgets(buffer, MAX_ROW_LEN, file) != NULL && counter < matrix -> numberOfRows && counter < row) counter ++;
     counter = 0;
     char *token = strtok(buffer, " ");
+
     while (token != NULL && counter < matrix -> numberOfColumns && counter < column){
         token = strtok(NULL, " ");
         counter ++;
@@ -204,9 +209,9 @@ void writeValueAtPosition(Matrix *matrix, int row, int column, FILE *file, int v
     int index = getIndex(matrix, row, column);
     char *number = calloc(matrix -> widthOfColumn, sizeof(char));
     sprintf(number, "%d", value);
-    int digs = floor(log10(abs(value))) + 1;
-    if (value < 0) digs ++;
-    for (int i = digs; i < matrix -> widthOfColumn -1; i++){
+    int digits = floor(log10(abs(value))) + 1;
+    if (value < 0) digits ++;
+    for (int i = digits; i < matrix -> widthOfColumn -1; i++){
         number[i] = ' ';
     }
     fseek(file, index, 0);
@@ -230,7 +235,47 @@ void multiplyColumn(Matrix *A, Matrix *B, Matrix*C, FILE *Afile, FILE *Bfile, FI
     }
 }
 
+// return name of additional file for given number of process
+char *getNameOfFile(int indexOfMatrix, int process){
+    char* path = calloc(FILENAME_MAX, sizeof(char));
+    char number[3];
+    sprintf(number, "%d", process);
+    strcpy(path, CC[indexOfMatrix] -> path);
+    strcat(path, "_");
+    strcat(path, number);
+    return path;
+}
 
+// crate additional files
+void createNewFiles(int indexOfMatrix, int startColumn, int numberOfColumnsPerProcess, int process){
+    FILE * A = fopen(AA[indexOfMatrix] -> path, "r");
+    FILE * B = fopen(BB[indexOfMatrix] -> path, "r");
+    if (A == NULL || B == NULL){
+        printf("Cannot open file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char*path = getNameOfFile(indexOfMatrix, process);
+    Matrix *newMatrix = createResultMatrix(path, CC[indexOfMatrix] -> numberOfRows, numberOfColumnsPerProcess);
+    FILE * file = fopen(path, "r+");
+
+    for (int i = 0; i < numberOfColumnsPerProcess; i ++){
+        for (int j = 0; j < AA[indexOfMatrix] -> numberOfRows; j ++){
+            int result = 0;
+            for (int c = 0; c < AA[indexOfMatrix] -> numberOfColumns; c++){
+                result += getValueAtPosition(AA[indexOfMatrix], j, c, A) * getValueAtPosition(BB[indexOfMatrix], c, startColumn + i, B);
+            }
+            writeValueAtPosition(newMatrix, j, i, file, result);
+        }
+    }
+    freeMatrix(newMatrix);
+    fclose(file);
+    fclose(A);
+    fclose(B);
+    free(path);
+}
+
+// mulitply all matrices
 int multiply(int process, int numberOfProcess, int timeLimit, int isDistinct){
     timerStart();
     int multiplyingCounter = 0;
@@ -252,7 +297,7 @@ int multiply(int process, int numberOfProcess, int timeLimit, int isDistinct){
             }
         }
         if (isDistinct == 1){
-            createNewFiles();
+            createNewFiles(i, startColumn, numberOfColumnsPerProcess, process);
         }
         else{
             FILE *A = fopen(AA[i] -> path, "r");
@@ -281,9 +326,56 @@ int multiply(int process, int numberOfProcess, int timeLimit, int isDistinct){
     exit(multiplyingCounter);
 }
 
-// TO DO
-void makePaste(){
+// create empty files which will be needed to paste
+void createEmptyFiles(int indexOfMatrix, int process){
+    for (int i = 0; i < process; i ++){
+        char* file = getNameOfFile(indexOfMatrix, process);
+        FILE * f = fopen(file, "w+");
+        fclose(f);
+        free(file);
+    }
+}
 
+// paste files to one file
+void makePaste(int *processesForMatrix, int numberOfProcess){
+    for (int i = 0; i < numberOfProcess; i++) {
+        wait(0);
+    }
+
+    for(int i = 0; i < numberOfPairs; i++){
+        char** arg = calloc(processesForMatrix[i] + 2, sizeof(char*));
+        arg[0] = (char*) calloc(6, sizeof(char));
+        strcpy(arg[0], "paste");
+
+        for (int j = 0; j < processesForMatrix[i]; j++){
+            arg[j + 1] = calloc(FILENAME_MAX, sizeof(char));
+            strcpy(arg[j + 1], getNameOfFile(i, j));
+        }
+        arg[processesForMatrix[i] + 1]=NULL;
+
+        pid_t childPid = fork();
+        if (childPid == 0){
+            int fd = open(CC[i] -> path, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+            dup2(fd,1); // system call creates a copy of a file descriptor
+            close(fd);
+            execvp("paste", arg);
+            strcpy(arg[0], "rm");
+        }
+        else wait(0);
+
+        // if this is comment additional files aren't remove
+        pid_t childPid2 = fork();
+        if (childPid2 == 0){
+            strcpy(arg[0], "rm");
+            execvp("rm", arg);
+        }
+        else wait(0);
+        //
+
+        for (int j = 0; j < processesForMatrix[i] + 1; j++) {
+            free(arg[j]);
+        }
+    }
 }
 
 int main(int argc, char** argv){
@@ -306,11 +398,15 @@ int main(int argc, char** argv){
     }
     
     readList(lista);
+    int *processesForMatrix = NULL;
 
     if (isDistinct == 1){
-        int *processesForMatrix = calloc(numberOfPairs, sizeof(int));
+        processesForMatrix = calloc(numberOfPairs, sizeof(int));
         for (int i = 0; i < numberOfPairs; i ++){
             processesForMatrix[i] = min(numberOfProcess, BB[i] -> numberOfColumns);
+        }
+        for (int i = 0; i < numberOfPairs; i ++){
+            createEmptyFiles(i, processesForMatrix[i]);
         }
     }
 
@@ -330,17 +426,18 @@ int main(int argc, char** argv){
     }
 
     if (isDistinct == 1){
-        makePaste(); //TODO
+        makePaste(processesForMatrix, numberOfProcess);
     }
 
     for (int i = 0; i < numberOfProcess; i++){
         int status;
         waitpid(childPids[i], &status, 0);
-        printf("The process with PID = %d have done %d multiplying operations\n", childPids[i], WEXITSTATUS(status));
+        printf("The process with PID = %d has done %d multiplying operations\n", childPids[i], WEXITSTATUS(status));
     }
 
 
     freeList();
+
 
     return 0;
 }
