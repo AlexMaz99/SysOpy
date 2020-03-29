33a6bc1d8f79982baf467fc5f59b4f80360197de
AlexMaz99 <ola_m10@onet.pl>
2020-03-29 10:52:51 +0200

    finished zad1: everything works + added comments

--- cw04/zad1/main.c
+++ cw04/zad1/main.c
@@ -6,11 +6,13 @@
 
 int stop = 0;
 
+// SIGINT - CTRL+C
 void signalSIGINT(int signum){
     printf("\nOdebrano sygnal SIGINT\n");
     exit(0);
 }
 
+// SIGTSTP - CTRL+Z
 void signalSTP(){
     if (stop == 0){
         printf("\nOczekuje na CTRL + Z - kontynuacja albo CTR + C - zakonczenie programu\n");
@@ -22,20 +24,22 @@ void signalSTP(){
 }
 
 int main(){
+    // set the disposition of the signal SIGINT to function signalSIGINT()
     signal(SIGINT, signalSIGINT);
 
+    // change the action taken by a process on receipt of signal SIGSTP
     struct sigaction act;
     act.sa_handler = signalSTP;
     act.sa_flags = 0;
-    sigemptyset(&act.sa_mask);
+    sigemptyset(&act.sa_mask); // initialize the signal set given by set to empty
     sigaction(SIGTSTP, &act, NULL);
 
     while(1){
         if (stop == 1){
-            pause();
+            pause(); // wait for signal
         }
         system("ls");
-        sleep(2);
+        sleep(2); // sleep process
     }
 
     return 0;
